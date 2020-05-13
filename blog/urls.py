7aae7328ce5331ee9bc6f765501a5049e360bf19
siren <sirentic@gmail.com>
2020-05-13 17:18:10 +0900

    image add

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
